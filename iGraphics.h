4360e7a26b5a453138dea7ab67459a72d46aa066
Anindya Biswas <parabolicanindya@gmail.com>
2024-11-23 20:42:57 +0600

    Update iGraphics.h

--- iGraphics.h
+++ iGraphics.h
@@ -7,8 +7,8 @@
 //  Version: 2.0.2012.2015
 //
 
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #pragma comment(lib, "glut32.lib")
 #pragma comment(lib, "glaux.lib")
 #include "GL/glut.h"
@@ -24,9 +24,9 @@
 
 int iScreenHeight, iScreenWidth;
 int iMouseX, iMouseY;
-int ifft=0;
-void (*iAnimFunction[10])(void)={0};
-int iAnimCount=0;
+int ifft = 0;
+void (*iAnimFunction[10])(void) = {0};
+int iAnimCount = 0;
 int iAnimDelays[10];
 int iAnimPause[10];
 
@@ -36,51 +36,109 @@ void iSpecialKeyboard(unsigned char);
 void iMouseMove(int, int);
 void iMouse(int button, int state, int x, int y);
 
-static void  __stdcall iA0(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[0])iAnimFunction[0]();}
-static void  __stdcall iA1(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[1])iAnimFunction[1]();}
-static void  __stdcall iA2(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[2])iAnimFunction[2]();}
-static void  __stdcall iA3(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[3])iAnimFunction[3]();}
-static void  __stdcall iA4(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[4])iAnimFunction[4]();}
-static void  __stdcall iA5(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[5])iAnimFunction[5]();}
-static void  __stdcall iA6(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[6])iAnimFunction[6]();}
-static void  __stdcall iA7(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[7])iAnimFunction[7]();}
-static void  __stdcall iA8(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[8])iAnimFunction[8]();}
-static void  __stdcall iA9(HWND,unsigned int, unsigned int, unsigned long){if(!iAnimPause[9])iAnimFunction[9]();}
+static void __stdcall iA0(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[0])
+        iAnimFunction[0]();
+}
+static void __stdcall iA1(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[1])
+        iAnimFunction[1]();
+}
+static void __stdcall iA2(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[2])
+        iAnimFunction[2]();
+}
+static void __stdcall iA3(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[3])
+        iAnimFunction[3]();
+}
+static void __stdcall iA4(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[4])
+        iAnimFunction[4]();
+}
+static void __stdcall iA5(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[5])
+        iAnimFunction[5]();
+}
+static void __stdcall iA6(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[6])
+        iAnimFunction[6]();
+}
+static void __stdcall iA7(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[7])
+        iAnimFunction[7]();
+}
+static void __stdcall iA8(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[8])
+        iAnimFunction[8]();
+}
+static void __stdcall iA9(HWND, unsigned int, unsigned int, unsigned long)
+{
+    if (!iAnimPause[9])
+        iAnimFunction[9]();
+}
 
 int iSetTimer(int msec, void (*f)(void))
 {
     int i = iAnimCount;
 
-    if(iAnimCount>=10){printf("Error: Maximum number of already timer used.\n");return -1;}
+    if (iAnimCount >= 10)
+    {
+        printf("Error: Maximum number of already timer used.\n");
+        return -1;
+    }
 
     iAnimFunction[i] = f;
     iAnimDelays[i] = msec;
     iAnimPause[i] = 0;
 
-    if(iAnimCount == 0) SetTimer(0, 0, msec, iA0);
-    if(iAnimCount == 1) SetTimer(0, 0, msec, iA1);
-    if(iAnimCount == 2) SetTimer(0, 0, msec, iA2);
-    if(iAnimCount == 3) SetTimer(0, 0, msec, iA3);
-    if(iAnimCount == 4) SetTimer(0, 0, msec, iA4);
+    if (iAnimCount == 0)
+        SetTimer(0, 0, msec, iA0);
+    if (iAnimCount == 1)
+        SetTimer(0, 0, msec, iA1);
+    if (iAnimCount == 2)
+        SetTimer(0, 0, msec, iA2);
+    if (iAnimCount == 3)
+        SetTimer(0, 0, msec, iA3);
+    if (iAnimCount == 4)
+        SetTimer(0, 0, msec, iA4);
 
-    if(iAnimCount == 5) SetTimer(0, 0, msec, iA5);
-    if(iAnimCount == 6) SetTimer(0, 0, msec, iA6);
-    if(iAnimCount == 7) SetTimer(0, 0, msec, iA7);
-    if(iAnimCount == 8) SetTimer(0, 0, msec, iA8);
-    if(iAnimCount == 9) SetTimer(0, 0, msec, iA9);
+    if (iAnimCount == 5)
+        SetTimer(0, 0, msec, iA5);
+    if (iAnimCount == 6)
+        SetTimer(0, 0, msec, iA6);
+    if (iAnimCount == 7)
+        SetTimer(0, 0, msec, iA7);
+    if (iAnimCount == 8)
+        SetTimer(0, 0, msec, iA8);
+    if (iAnimCount == 9)
+        SetTimer(0, 0, msec, iA9);
     iAnimCount++;
 
-    return iAnimCount-1;
+    return iAnimCount - 1;
 }
 
-void iPauseTimer(int index){
-    if(index>=0 && index <iAnimCount){
+void iPauseTimer(int index)
+{
+    if (index >= 0 && index < iAnimCount)
+    {
         iAnimPause[index] = 1;
     }
 }
 
-void iResumeTimer(int index){
-    if(index>=0 && index <iAnimCount){
+void iResumeTimer(int index)
+{
+    if (index >= 0 && index < iAnimCount)
+    {
         iAnimPause[index] = 0;
     }
 }
@@ -103,7 +161,7 @@ void iResumeTimer(int index){
 //
 void iShowBMP2(int x, int y, char filename[], int ignoreColor)
 {
-   /// old implementation, causes memory leak
+    /// old implementation, causes memory leak
     /*
 
     AUX_RGBImageRec *TextureImage;
@@ -135,85 +193,82 @@ void iShowBMP2(int x, int y, char filename[], int ignoreColor)
     free(TextureImage);  // this free doesn't work
     */
 
-/// this implementation doesn't work for some bmp files
-//    int i,j;
-//
-//    FILE* f = fopen(filename, "rb");
-//    unsigned char info[54];
-//    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
-//
-//    // extract image height and width from header
-//    int width = *(int*)&info[18];
-//    int height = *(int*)&info[22];
-//
-//    printf("width: %d, height: %d\n",width,height);
-//
-//    int nPixels = width * height;
-//    int *rgPixels = new int[nPixels];
-//
-//    int size = 3 * nPixels;
-//    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
-//    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
-//    fclose(f);
-//
-//
-//    for (i = 0, j=0; i < nPixels; i++, j += 3)
-//    {
-//        int bgr = 0;
-//
-//        //for(int k = 2; k >= 0; k--)
-//        for(int k = 0; k < 3; k++)
-//        {
-//
-//            bgr = ((bgr << 8) | data[j+k]);
-//
-//        }
-//
-//        printf("%x\n",bgr);
-//
-//        rgPixels[i] = (bgr == ignoreColor) ? 0 : 255;
-//
-//        rgPixels[i] = ((rgPixels[i] << 24) | bgr);
-//    }
-//
-//    delete []data;
-//
-//    glRasterPos2f(x, y);
-//    glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, rgPixels);
-//
-//    delete []rgPixels;
+    /// this implementation doesn't work for some bmp files
+    //    int i,j;
+    //
+    //    FILE* f = fopen(filename, "rb");
+    //    unsigned char info[54];
+    //    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+    //
+    //    // extract image height and width from header
+    //    int width = *(int*)&info[18];
+    //    int height = *(int*)&info[22];
+    //
+    //    printf("width: %d, height: %d\n",width,height);
+    //
+    //    int nPixels = width * height;
+    //    int *rgPixels = new int[nPixels];
+    //
+    //    int size = 3 * nPixels;
+    //    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
+    //    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+    //    fclose(f);
+    //
+    //
+    //    for (i = 0, j=0; i < nPixels; i++, j += 3)
+    //    {
+    //        int bgr = 0;
+    //
+    //        //for(int k = 2; k >= 0; k--)
+    //        for(int k = 0; k < 3; k++)
+    //        {
+    //
+    //            bgr = ((bgr << 8) | data[j+k]);
+    //
+    //        }
+    //
+    //        printf("%x\n",bgr);
+    //
+    //        rgPixels[i] = (bgr == ignoreColor) ? 0 : 255;
+    //
+    //        rgPixels[i] = ((rgPixels[i] << 24) | bgr);
+    //    }
+    //
+    //    delete []data;
+    //
+    //    glRasterPos2f(x, y);
+    //    glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, rgPixels);
+    //
+    //    delete []rgPixels;
 
-
-/// this probably works for all types of bmp files
-    int  width, height, n;
+    /// this probably works for all types of bmp files
+    int width, height, n;
     stbi_set_flip_vertically_on_load(1);
-    unsigned char* image = stbi_load(filename, &width, &height, &n, 4);
+    unsigned char *image = stbi_load(filename, &width, &height, &n, 4);
 
     int nPixels = width * height;
     int *rgPixels = new int[nPixels];
 
-    int i,j;
-    for (i = 0, j=0; i < nPixels; i++, j += 4)
+    int i, j;
+    for (i = 0, j = 0; i < nPixels; i++, j += 4)
     {
         int bgr = 0;
-        for(int k = 2; k >= 0; k--)
+        for (int k = 2; k >= 0; k--)
         {
-            bgr = ((bgr << 8) | image[j+k]);
-
+            bgr = ((bgr << 8) | image[j + k]);
         }
-        //printf("%x\n",bgr);
+        // printf("%x\n",bgr);
 
         rgPixels[i] = (bgr == ignoreColor) ? 0 : 255;
         rgPixels[i] = ((rgPixels[i] << 24) | bgr);
     }
 
     glRasterPos2f(x, y);
-    //glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    // glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, image);
     glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, rgPixels);
     delete[] rgPixels;
 
     stbi_image_free(image);
-
 }
 
 void iShowBMP(int x, int y, char filename[])
@@ -221,36 +276,37 @@ void iShowBMP(int x, int y, char filename[])
     iShowBMP2(x, y, filename, -1 /* ignoreColor */);
 }
 
-void iGetPixelColor (int cursorX, int cursorY, int rgb[])
+void iGetPixelColor(int cursorX, int cursorY, int rgb[])
 {
     GLubyte pixel[3];
-    glReadPixels(cursorX, cursorY,1,1,
-        GL_RGB,GL_UNSIGNED_BYTE,(void *)pixel);
+    glReadPixels(cursorX, cursorY, 1, 1,
+                 GL_RGB, GL_UNSIGNED_BYTE, (void *)pixel);
 
     rgb[0] = pixel[0];
     rgb[1] = pixel[1];
     rgb[2] = pixel[2];
 
-    //printf("%d %d %d\n",pixel[0],pixel[1],pixel[2]);
+    // printf("%d %d %d\n",pixel[0],pixel[1],pixel[2]);
 }
 
-void iText(GLdouble x, GLdouble y, char *str, void* font=GLUT_BITMAP_8_BY_13)
+void iText(GLdouble x, GLdouble y, char *str, void *font = GLUT_BITMAP_8_BY_13)
 {
     glRasterPos3d(x, y, 0);
     int i;
-    for (i=0; str[i]; i++) {
+    for (i = 0; str[i]; i++)
+    {
         glutBitmapCharacter(font, str[i]); //,GLUT_BITMAP_8_BY_13, GLUT_BITMAP_TIMES_ROMAN_24
     }
 }
 
-void iPoint(double x, double y, int size=0)
+void iPoint(double x, double y, int size = 0)
 {
     int i, j;
     glBegin(GL_POINTS);
     glVertex2f(x, y);
-    for(i=x-size;i<x+size;i++)
+    for (i = x - size; i < x + size; i++)
     {
-        for(j=y-size; j<y+size;j++)
+        for (j = y - size; j < y + size; j++)
         {
             glVertex2f(i, j);
         }
@@ -269,9 +325,11 @@ void iLine(double x1, double y1, double x2, double y2)
 void iFilledPolygon(double x[], double y[], int n)
 {
     int i;
-    if(n<3)return;
+    if (n < 3)
+        return;
     glBegin(GL_POLYGON);
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; i++)
+    {
         glVertex2f(x[i], y[i]);
     }
     glEnd();
@@ -280,9 +338,11 @@ void iFilledPolygon(double x[], double y[], int n)
 void iPolygon(double x[], double y[], int n)
 {
     int i;
-    if(n<3)return;
+    if (n < 3)
+        return;
     glBegin(GL_LINE_STRIP);
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; i++)
+    {
         glVertex2f(x[i], y[i]);
     }
     glVertex2f(x[0], y[0]);
@@ -295,8 +355,8 @@ void iRectangle(double left, double bottom, double dx, double dy)
 
     x1 = left;
     y1 = bottom;
-    x2=x1+dx;
-    y2=y1+dy;
+    x2 = x1 + dx;
+    y2 = y1 + dy;
 
     iLine(x1, y1, x2, y1);
     iLine(x2, y1, x2, y2);
@@ -311,29 +371,29 @@ void iFilledRectangle(double left, double bottom, double dx, double dy)
 
     x1 = left;
     y1 = bottom;
-    x2=x1+dx;
-    y2=y1+dy;
+    x2 = x1 + dx;
+    y2 = y1 + dy;
 
-    xx[0]=x1;
-    yy[0]=y1;
-    xx[1]=x2;
-    yy[1]=y1;
-    xx[2]=x2;
-    yy[2]=y2;
-    xx[3]=x1;
-    yy[3]=y2;
+    xx[0] = x1;
+    yy[0] = y1;
+    xx[1] = x2;
+    yy[1] = y1;
+    xx[2] = x2;
+    yy[2] = y2;
+    xx[3] = x1;
+    yy[3] = y2;
 
     iFilledPolygon(xx, yy, 4);
 }
 
-void iFilledCircle(double x, double y, double r, int slices=100)
+void iFilledCircle(double x, double y, double r, int slices = 100)
 {
-    double t, PI=acos(-1.0), dt, x1,y1, xp, yp;
-    dt = 2*PI/slices;
-    xp = x+r;
+    double t, PI = acos(-1.0), dt, x1, y1, xp, yp;
+    dt = 2 * PI / slices;
+    xp = x + r;
     yp = y;
     glBegin(GL_POLYGON);
-    for(t = 0; t <= 2*PI; t+=dt)
+    for (t = 0; t <= 2 * PI; t += dt)
     {
         x1 = x + r * cos(t);
         y1 = y + r * sin(t);
@@ -345,13 +405,13 @@ void iFilledCircle(double x, double y, double r, int slices=100)
     glEnd();
 }
 
-void iCircle(double x, double y, double r, int slices=100)
+void iCircle(double x, double y, double r, int slices = 100)
 {
-    double t, PI=acos(-1.0), dt, x1,y1, xp, yp;
-    dt = 2*PI/slices;
-    xp = x+r;
+    double t, PI = acos(-1.0), dt, x1, y1, xp, yp;
+    dt = 2 * PI / slices;
+    xp = x + r;
     yp = y;
-    for(t = 0; t <= 2*PI; t+=dt)
+    for (t = 0; t <= 2 * PI; t += dt)
     {
         x1 = x + r * cos(t);
         y1 = y + r * sin(t);
@@ -361,13 +421,13 @@ void iCircle(double x, double y, double r, int slices=100)
     }
 }
 
-void iEllipse(double x, double y, double a, double b, int slices=100)
+void iEllipse(double x, double y, double a, double b, int slices = 100)
 {
-    double t, PI=acos(-1.0), dt, x1,y1, xp, yp;
-    dt = 2*PI/slices;
-    xp = x+a;
+    double t, PI = acos(-1.0), dt, x1, y1, xp, yp;
+    dt = 2 * PI / slices;
+    xp = x + a;
     yp = y;
-    for(t = 0; t <= 2*PI; t+=dt)
+    for (t = 0; t <= 2 * PI; t += dt)
     {
         x1 = x + a * cos(t);
         y1 = y + b * sin(t);
@@ -377,14 +437,14 @@ void iEllipse(double x, double y, double a, double b, int slices=100)
     }
 }
 
-void iFilledEllipse(double x, double y, double a, double b, int slices=100)
+void iFilledEllipse(double x, double y, double a, double b, int slices = 100)
 {
-    double t, PI=acos(-1.0), dt, x1,y1, xp, yp;
-    dt = 2*PI/slices;
-    xp = x+a;
+    double t, PI = acos(-1.0), dt, x1, y1, xp, yp;
+    dt = 2 * PI / slices;
+    xp = x + a;
     yp = y;
     glBegin(GL_POLYGON);
-    for(t = 0; t <= 2*PI; t+=dt)
+    for (t = 0; t <= 2 * PI; t += dt)
     {
         x1 = x + a * cos(t);
         y1 = y + b * sin(t);
@@ -436,10 +496,13 @@ void iSetColor(double r, double g, double b)
 {
     double mmx;
     mmx = r;
-    if(g > mmx)mmx = g;
-    if(b > mmx)mmx = b;
+    if (g > mmx)
+        mmx = g;
+    if (b > mmx)
+        mmx = b;
     mmx = 255;
-    if(mmx > 0){
+    if (mmx > 0)
+    {
         r /= mmx;
         g /= mmx;
         b /= mmx;
@@ -451,30 +514,33 @@ void iDelay(int sec)
 {
     int t1, t2;
     t1 = time(0);
-    while(1){
+    while (1)
+    {
         t2 = time(0);
-        if(t2-t1>=sec)
+        if (t2 - t1 >= sec)
             break;
     }
 }
 
 void iClear()
 {
-    glClear(GL_COLOR_BUFFER_BIT) ;
-    glMatrixMode(GL_MODELVIEW) ;
-    glClearColor(0,0,0,0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glClearColor(0, 0, 0, 0);
     glFlush();
 }
 
-void displayFF(void){
+void displayFF(void)
+{
 
     iDraw();
-    glutSwapBuffers() ;
+    glutSwapBuffers();
 }
 
 void animFF(void)
 {
-    if(ifft == 0){
+    if (ifft == 0)
+    {
         ifft = 1;
         iClear();
     }
@@ -511,37 +577,37 @@ void mouseHandlerFF(int button, int state, int x, int y)
     glFlush();
 }
 
-void iInitialize(int width=500, int height=500, char *title="iGraphics")
+void iInitialize(int width = 500, int height = 500, char *title = "iGraphics")
 {
     iScreenHeight = height;
     iScreenWidth = width;
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_ALPHA) ;
-    glutInitWindowSize(width , height ) ;
-    glutInitWindowPosition( 10 , 10 ) ;
-    glutCreateWindow(title) ;
-    glClearColor( 0.0 , 0.0 , 0.0 , 0.0 ) ;
-    glMatrixMode( GL_PROJECTION) ;
-    glLoadIdentity() ;
-    glOrtho(0.0 , width , 0.0 , height , -1.0 , 1.0) ;
-    //glOrtho(-100.0 , 100.0 , -100.0 , 100.0 , -1.0 , 1.0) ;
-    //SetTimer(0, 0, 10, timer_proc);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_ALPHA);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(10, 10);
+    glutCreateWindow(title);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, width, 0.0, height, -1.0, 1.0);
+    // glOrtho(-100.0 , 100.0 , -100.0 , 100.0 , -1.0 , 1.0) ;
+    // SetTimer(0, 0, 10, timer_proc);
 
     iClear();
 
-    glutDisplayFunc(displayFF) ;
-    glutKeyboardFunc(keyboardHandler1FF); //normal
-    glutSpecialFunc(keyboardHandler2FF); //special keys
+    glutDisplayFunc(displayFF);
+    glutKeyboardFunc(keyboardHandler1FF); // normal
+    glutSpecialFunc(keyboardHandler2FF);  // special keys
     glutMouseFunc(mouseHandlerFF);
     glutMotionFunc(mouseMoveHandlerFF);
-    glutIdleFunc(animFF) ;
+    glutIdleFunc(animFF);
 
     //
     // Setup Alpha channel testing.
     // If alpha value is greater than 0, then those
     // pixels will be rendered. Otherwise, they would not be rendered
     //
-    glAlphaFunc(GL_GREATER,0.0f);
+    glAlphaFunc(GL_GREATER, 0.0f);
     glEnable(GL_ALPHA_TEST);
 
     glutMainLoop();
